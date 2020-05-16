3c2f15d388858ffcb8f80e7b4b3de935c0e5daf2
Paul Ferguson <paullferguson@gmail.com>
2020-05-16 15:09:15 -0700

    Final IP address

--- server/index.js
+++ server/index.js
@@ -9,13 +9,9 @@ ProxyApp.set('port', 3000);
 const apiProxy = httpProxy.createProxyServer();
 
 // Local
-let qnaService = '';
-let decService = '';
-let revService = '';
-
-if (process.env.SERVICE_1) qnaService = process.env.SERVICE_1;
-if (process.env.SERVICE_2) decService = process.env.SERVICE_2;
-if (process.env.SERVICE_3) revService = process.env.SERVICE_3;
+let qnaService = "http://54.184.20.122:3002";
+let decService = "http://34.212.75.246:3001";
+let revService = "http://44.228.132.116:3003";
 
 
 // The Middle
@@ -35,11 +31,18 @@ ProxyApp.use('/:id', express.static('public'));
 ProxyApp.all('/questions-service/*', (req, resp) => {
   console.log('Proxy to Q and A server');
   apiProxy.web(req, resp, {target: qnaService});
+  apiProxy.on('error', function(e) {
+    console.log('Proxy loading ERROR:', e);
+    // resp.sendStatus(500);
+  });
 });
 
 ProxyApp.all('/questions/*', (req, resp) => {
   console.log('Proxy to Q and A server API');
   apiProxy.web(req, resp, {target: qnaService});
+  apiProxy.on('error', function(e) {
+    resp.status(500).send(e);
+  });
 });
 
 
@@ -48,16 +51,25 @@ ProxyApp.all('/questions/*', (req, resp) => {
 ProxyApp.all('/description-service/*', (req, resp) => {
   console.log('Proxy to Description server');
   apiProxy.web(req, resp, {target: decService});
+  apiProxy.on('error', function(e) {
+    resp.status(500).send(e);
+  });
 });
 
 ProxyApp.all('/getallproducts/*', (req, resp) => {
   console.log('Proxy to Description server API');
   apiProxy.web(req, resp, {target: decService});
+  apiProxy.on('error', function(e) {
+    resp.status(500).send(e);
+  });
 });
 
 ProxyApp.all('/getsingleproduct/*', (req, resp) => {
   console.log('Proxy to Description server API');
   apiProxy.web(req, resp, {target: decService});
+  apiProxy.on('error', function(e) {
+    resp.status(500).send(e);
+  });
 });
 
 
@@ -66,11 +78,17 @@ ProxyApp.all('/getsingleproduct/*', (req, resp) => {
 ProxyApp.all('/review-service/*', (req, resp) => {
   console.log('Proxy to Review server');
   apiProxy.web(req, resp, {target: revService});
+  apiProxy.on('error', function(e) {
+    resp.status(500).send(e);
+  });
 });
 
 ProxyApp.all('/review-api/*', (req, resp) => {
   console.log('Proxy to Review server API');
   apiProxy.web(req, resp, {target: revService});
+  apiProxy.on('error', function(e) {
+    resp.status(500).send(e);
+  });
 });
 
 
